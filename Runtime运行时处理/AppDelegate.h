0c9c679f64153e71e07243935d4ef30fd55909f2
Sun Jiaqi <583142225@qq.com>
2015-03-14 14:11:30 +0800

    Initial Commit

---
+++ Runtime运行时处理/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Runtime运行时处理
+//
+//  Created by mac on 15-3-14.
+//  Copyright (c) 2015年 mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Runtime运行时处理/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Runtime运行时处理
+//
+//  Created by mac on 15-3-14.
+//  Copyright (c) 2015年 mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
